71af4d47cc804d6c20a90bb053bfc08ba4b44077
Strads <stradssteen@gmail.com>
2015-10-22 15:15:21 +0200

    Update getGaussFilt.m
    Updated to have the weird size of 3*SD, as seen in exercise text. Other version used 5 SD in each direction, catching 99.9999426697% of the gaussian information.

--- getGaussFilt.m
+++ getGaussFilt.m
@@ -1,6 +1,6 @@
 function [ g dG ddG dddG x ] = getGaussFilt(t)
 
-x = round(-5*sqrt(t)):1:round(5*sqrt(t));
+x = -(3*t-1)/2:1:(3*t-1)/2;
 g = 1/sqrt(2*pi*t)*exp(-x.^2/(2*t));
 dG = -x/sqrt(2*pi*t^3).*exp(-x.^2/(2*t));
 ddG = (x-sqrt(t)).*(x+sqrt(t))/sqrt(2*pi*t^5).*exp(-x.^2/(2*t));
