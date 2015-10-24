d952a1ad0da4137ff8aae1851c7b7ab33f5e7f3f
Anton Troynikov <Anton Troynikov@192.168.0.10>
2015-10-24 12:41:54 +0200

    Added comments and made variable names clearer. Filter output should produce an image we can plot immediately.

--- exercise1.m
+++ exercise1.m
@@ -1,6 +1,15 @@
+%Initialize the workspace
 clear all; clc; close all;
-I = imread('lena.gif');
-figure; subplot(1,2,1); imagesc(double(I));
-h = ones(3,3);
-II = filter2d(I, h, 'replicate');
-subplot(1,2,2); imagesc(II'); colormap gray;
+
+%Read in and plot the input image
+input = imread('lena.gif');
+figure; subplot(1,2,1); imagesc(double(input));
+
+%Create the input mask
+mask = ones(3,3);
+
+%Apply the convolution filter
+output = filter2d(input, mask, 'replicate');
+
+%Plot the output
+subplot(1,2,2); imagesc(output); colormap gray;
